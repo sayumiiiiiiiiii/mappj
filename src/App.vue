21f275efe87fca87e9c6539b29dee48950d0e7aa
sayumiiiiiiiiii <ide.sayumi.0116@gmail.com>
2022-04-21 16:41:57 +0900

    mappj

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GoogleMap />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GoogleMap from "./components/GoogleMap"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GoogleMap
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as VueGoogleMaps from "vue2-google-maps"
 
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key:"AIzaSyCCdBMbyv2BWnzi0frpRk-exwW2qUDc6CU",
+    libraries: "places", // necessary for places input
+    region: 'JP',
+    language: 'ja'
+  }
+});
 
 new Vue({
   render: h => h(App),
